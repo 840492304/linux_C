0e43ef0b10d4c854d97e4fc034d43f8ab7627850
wen <840492304@qq.com>
2018-03-05 17:43:44 +0800

    文件描述符重定向--子进程跟父进程之间的关系

---
+++ redirectInput3.c
@@ -0,0 +1,46 @@
+//
+// Created by Wen on 2018/3/5.
+//
+#include <stdio.h>
+#include <zconf.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
+void main(){
+
+    int pid = 0, fd;
+
+    printf("接下来表演的是如何将子进程的文件描述符1重定向\n");
+
+    /**
+     * fork子进程
+     */
+    if ((pid == fork()) == -1) {
+        perror("fork出错啦");
+        exit(1);
+    }
+
+
+    /**
+     * 这是子进程
+     */
+    if (pid == 0) {
+
+        close(1); //关闭
+
+        creat("a", 0666);
+
+        execlp("who", "who", NULL);
+
+        exit(1);
+
+    }
+
+
+    if (pid != 0) {
+
+        wait(NULL);
+        printf("over!");
+    }
+
+}
