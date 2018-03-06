975908004f0298b3b6a8afc2ad3ee04c03c1e19d
wen <840492304@qq.com>
2018-03-06 11:41:01 +0800

    pipe的fork版本

--- pipeDemo1.c
+++ pipeDemo1.c
@@ -5,7 +5,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+/**
+ * 为什么 pipe不能将标准输出重定向到pipe[0]上？这样管道输入端直接输入，就直接输出了
+ *
+ * 关闭pi[0], 发送数据，会触发EPIPE信号,程序退出。啊哈哈
+ *
+ *
+ */
 /**
  * pipe的Demo
  */
@@ -29,6 +35,7 @@ void main(){
 //    close(1);
 //
 //    dup(pi[0]);
+//    close(pi[0]);
 
 
     while ((fgets(inputs, 100, stdin)) != NULL){
@@ -47,14 +54,14 @@ void main(){
             inputs[i] = 'X';
         }
 
-        /**
-         * 读取管道数据
-         */
+//        /**
+//         * 读取管道数据
+//         */
         read(pi[0], inputs, len);
-
-        /**
-         * 发送给标准输出
-         */
+//
+//        /**
+//         * 发送给标准输出
+//         */
         write(1, inputs, len);
 
     }
---
+++ pipeDemo2fork.c
@@ -0,0 +1,85 @@
+//
+// Created by Wen on 2018/3/6.
+//
+#include <zconf.h>
+#include <stdio.h>
+#include <string.h>
+
+
+#define inputs "werftyguhi\n"
+/**
+ * 使用fork
+ */
+void main(){
+
+    int pi[2], pid, len;
+
+    char gets[100];
+
+    if(pipe(pi) != 0){
+
+        perror("pipe出错");
+    }
+
+
+    pid = fork();
+
+    printf("pid is %d \n", pid);
+    printf("pipe文件描述符  为 %d %d \n", pi[0], pi[1]);
+
+    len = strlen(inputs);
+
+    if (pid == 0) {
+
+        close(pi[0]);
+        while (1){
+
+
+            /**
+             * pipe管道发送数据
+             */
+            if (write(pi[1], inputs, len) != len) {
+
+                printf("出错啦");
+            }
+
+            sleep(5);
+        }
+    }
+
+
+    if (pid != 0) {
+        close(pi[1]);
+
+        while (1){
+
+
+//        /**
+//         * 读取管道数据
+//         */
+            read(pi[0], gets, len);
+//
+//        /**
+//         * 发送给标准输出
+//         */
+            write(1, gets, len);
+
+            for (int i= 0; i < len; ++i) {
+
+                gets[i] = 'x';
+            }
+
+
+         //   printf("%s", inputs);
+
+            sleep(2);
+
+        }
+    }
+
+
+
+
+
+}
+
