a1b28f56bff82dc2fc71deffa41ec3c04c0f3979
wen <840492304@qq.com>
2018-03-16 14:14:12 +0800

    udp服务器 交互

--- udp/client.c
+++ udp/client.c
@@ -1,6 +1,10 @@
-//
-// Created by Wen on 2018/3/15.
-//
+
+/**
+ * recvfrom(socket_id, buf, BUFFSIZE, 0,  &saddr_in_receve, &addr_len)
+ *
+ * 这个长度。。。int * 类型的
+ *
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,28 +12,51 @@
 #include <memory.h>
 #include "dgram.h"
 
-#define oops(m, n) {perror(m); exit(n);}
+#define BUFFSIZE 1024
 
+#define oops(m, n) {perror(m); exit(n);}
 
 int main(int ac, char *av[]) {
 
+    char buf[BUFFSIZE];
+    int rece_num;
+
+    int addr_len;
+
     int socket_id;
     struct sockaddr_in saddr_in;
-
+    struct sockaddr_in saddr_in_receve;
     if(ac != 3) oops("参数有误", 1);
 
-    if ((socket_id = make_client_udp(atoi(av[1]))) == -1) oops("socket申请失败", 2);
+    /**
+     *
+     */
+    if ((socket_id = make_server_udp(4445)) == -1) oops("socket申请失败", 2);
+
+
+    printf("socket%d\n", socket_id);
 
     if ( make_server_addr(atoi(av[1]), &saddr_in) == -1) oops("地址失败", 3);
 
     if (sendto(socket_id, av[2], strlen(av[2]), 0, (struct sockaddr *) &saddr_in, sizeof(saddr_in)) == -1) {
 
-        printf("发送失败");
+        //printf("发送失败");
+        perror("发送失败");
 
         return 1;
     }
 
-    printf("发送成功!");
+    printf("发送成功!等待接收中\n");
+
+    addr_len = sizeof(saddr_in_receve);
+
+    if((rece_num = recvfrom(socket_id, buf, BUFFSIZE, 0,  &saddr_in_receve, &addr_len)) > 0){
+
+        buf[rece_num] = '\0';
+        printf("接收到数据为:%s", buf);
+    }
+
+    printf("%d\n", socket_id);
 
     return 0;
 }
--- udp/server1.c
+++ udp/server1.c
@@ -2,17 +2,27 @@
 // Created by Wen on 2018/3/15.
 //
 
+/**
+ * recvfrom(socket_id, buf, BUFFSIZE, 0,  &saddr_in_receve, &addr_len)
+ *
+ * 这个长度。。。int * 类型的
+ *
+ */
+
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <memory.h>
 
 #include "dgram.h"
 
 #define oops(m, n) {perror(m); exit(n);}
 #define BUFFSIZE 1024
 
-
+void send_to_client(char * msg, int socked_id, struct sockaddr_in *adrr);
 
 int main(int ac, char *av[]) {
 
@@ -33,18 +43,14 @@ int main(int ac, char *av[]) {
 
     socket_id = make_server_udp(port);
 
-   // make_server_addr(port, &saddr_in);
+    printf("监听端口号为%d\n", port);
+    printf("socket_id为%d\n", socket_id);
 
     addr_len = sizeof(saddr_in);
 
-
-    while (1) {
-
+    while (1){
 
         if((receve_num = recvfrom(socket_id, buf, BUFFSIZE, 0,  &saddr_in,  &addr_len)) > 0){
-
-
-            printf("-----接受----");
 
             buf[receve_num] = '\0';
 
@@ -52,14 +58,15 @@ int main(int ac, char *av[]) {
 
             print_client(&saddr_in);
 
+            send_to_client(buf, socket_id, &saddr_in);
+
+            printf("发送完毕\n");
+
+
         }
-
-
     }
 
-
-
-    printf("结束");
+    printf("结束\n");
 
     return 0;
 }
@@ -74,8 +81,28 @@ void print_client(struct sockaddr_in  * saddr){
 
     printf("当前客户端 IP为 %s 当前端口为 %d\n", host, port);
 
+
+
 }
 
 
+void send_to_client(char * msg, int socked_id, struct sockaddr_in *adrr){
+
+    char res[1024];
+    int send_num;
+
+    sprintf(res, "I has received your msg:%s\n", msg);
+
+  //  if ( make_server_addr(4445, adrr) == -1) oops("地址失败", 3);
+
+    if ((send_num = sendto(socked_id, res, sizeof( res), 0, adrr, sizeof(adrr)))  <= 0){
+
+        oops("返回信息错误", 3);
+    }else{
+
+        printf("发送完毕1\n");
+    }
+
+}
 
 
